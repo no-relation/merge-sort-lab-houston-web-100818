9d04ecc2b0430d6679ae38606f3863dc8b306ae4
no-relation <eddiec76@gmail.com>
2019-02-01 19:27:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
 
+function merge(array1, array2) {
+    let newArray = []
+    while (array1.length > 0 && array2.length > 0) {
+        if (array1[0] < array2[0]) {
+            newArray.push(array1.shift())
+        } else {
+            newArray.push(array2.shift())
+        }
+    }
+    return newArray.concat(array1).concat(array2)
+}
+
+function mergeSort(array) {
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0,midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+
+    if (array.length < 2) {
+        return array
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
